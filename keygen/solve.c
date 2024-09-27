bbe3c8e5498be080c7023dade670c033462dfd41
Dhia Elhak EZZEDDINI <155757052+DhiaElhak-Ezzeddini@users.noreply.github.com>
2024-09-27 10:02:33 -0400

    Update solve.c

--- keygen/solve.c
+++ keygen/solve.c
@@ -2,7 +2,6 @@
 #include <strings.h>
 int main(void){
 	unsigned char name[] = "aaaaaaaaaaaaaaaaaa" ; /* put what ever you want , just ensure it is <=20 bytes */
-        unsigned char pass[4] ; 
 	printf("cracked key : ") ; 	
 	for (int i=0 ; i<sizeof(name) ; i++){
 		char s = (name[i] ^ 0x1)  + (char)i ; 
